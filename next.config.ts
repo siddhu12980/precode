563b6d11782adec076c4080467a72659e0efbbeb
Siddhanta Acharya <siddhu12980@gmail.com>
2026-06-24 02:25:47 +0530

    fix: client generation bg

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  outputFileTracingIncludes: {
-    "/*": ["./app/generated/prisma/**/*"],
-  },
 };
 
 export default nextConfig;
